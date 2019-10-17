bc853b970e434325fa1adea96ee7ffafbe58fc9b
ruthtech <leeruths@gmail.com>
2019-10-17 12:41:28 -0400

    Generalize the map load and school adding.

---
+++ ruth/loadMap.js
@@ -0,0 +1,55 @@
+const schoolLat =  43.7765020619;
+const schoolLon = -79.3851899865;
+
+function createMap() {
+	// Later, after the map has been populated with many points, centre the map on the main Toronto coordinates.
+	const torontoLat = 43.7;
+	const torontoLon = -79.42;
+
+	// 9 shows Toronto and surrounding areas. 
+	// 12 shows the city. 
+	// 15 zooms in to Eglinton and Bathurst. 
+	let zoomLevel = 12;
+	
+	mapboxgl.accessToken = Login.retrieveAPIKey();
+	let map = new mapboxgl.Map({
+		  container: 'map', // HTML container id
+		  style: 'mapbox://styles/mapbox/streets-v11', // style URL
+		  center: [schoolLon, schoolLat], // starting position as [lng, lat]
+		  zoom: zoomLevel
+		});
+	
+	// loop over the schools
+	let schools = getSchools();
+	for(let i=0; i<schools.length; i++) {
+		let school = schools[i];
+		let marker = addMarkerToMap(map, school);
+		marker.addTo(map);
+	}
+}
+
+function getSchools() {
+	return [createTempSchool()];
+}
+
+function createTempSchool() {
+	let school = 
+	{
+		name: "A School",
+		desc: "A really good school",
+		lon: schoolLat,
+		lat: schoolLon		
+	};
+	
+	return school;
+}
+	
+function createMarker( school ) {
+	let popup = new mapboxgl.Popup()
+	  .setHTML('<h3>${school.name}</h3><p>${school.desc}</p>');
+
+	let marker = new mapboxgl.Marker()
+	  .setLngLat([school.lon, school.lat])
+	  .setPopup(popup);
+
+}
