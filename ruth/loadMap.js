6faa3f6270091729a197a98ab70bd2187607b33e
ruthtech <leeruths@gmail.com>
2019-10-17 16:37:35 -0400

    Add the schools into the map. Note: takes a long time. Rethink this.

--- ruth/loadMap.js
+++ ruth/loadMap.js
@@ -1,11 +1,10 @@
-const schoolLat =  43.7765020619;
-const schoolLon = -79.3851899865;
-
-function createMap( stevesSchools ) {
+let x = function createMap( schools ) {
+	if(!schools) {
+		console.log("createMap. Error. Schools must be initialized.");
+	}
 	// Later, after the map has been populated with many points, centre the map on the main Toronto coordinates.
 	const torontoLat = 43.7;
 	const torontoLon = -79.42;
-	console.log("louuurd");
 
 	// 9 shows Toronto and surrounding areas. 
 	// 12 shows the city. 
@@ -16,46 +15,33 @@ function createMap( stevesSchools ) {
 	let map = new mapboxgl.Map({
 		  container: 'map', // HTML container id
 		  style: 'mapbox://styles/mapbox/streets-v11', // style URL
-		  center: [schoolLon, schoolLat], // starting position as [lng, lat]
+		  center: [torontoLon, torontoLat], // starting position as [lng, lat]
 		  zoom: zoomLevel
 		});
 	
 	// loop over the schools
-	let schools = getSchools();
-	console.log("here");
-	console.log(schools);
 	for(let i=0; i<schools.length; i++) {
 		let school = schools[i];
 		let marker = createMarker(school, map);
 	}
 }
+fetchSchools( x );		
 
 function createMarker( school, map ) {
+	let popupHTML = `
+		<h3>${school.name}</h3>
+		<h4>Alternative name: ${school.nameAlt}</h4>
+		<p>Municipality: ${school.municipality}</p>
+		<p>${school.addressFull}</p>
+		<p>${school.typeDesc}</p>
+	`;
 	let popup = new mapboxgl.Popup()
-	  .setHTML('<h3>${school.name}</h3><p>${school.desc}</p>');
+	  .setHTML(popupHTML);
 
 	let marker = new mapboxgl.Marker()
-	  .setLngLat([school.lon, school.lat])
+	  .setLngLat([school.longitude, school.latitude])
 	  .setPopup(popup)
 	  .addTo(map);
 
 	return marker;
-}
-
-function getSchools() {
-	let schools = [createTempSchool()];
-	console.log(schools);
-	return schools;
-}
-
-function createTempSchool() {
-	let school = 
-	{
-		name: "A School",
-		desc: "A really good school",
-		lon: schoolLon,
-		lat: schoolLat		
-	};
-	console.log(school);
-	return school;
 }
